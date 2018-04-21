e6f1e53587adf56a42f40c7a69a6ca83558b8ea2
SANVEER <sanveers7@gmail.com>
2018-04-21 04:12:38 +0200

    Designed home screen

--- app/src/main/java/com/unsignedcharacters/dassie/MainActivity.kt
+++ app/src/main/java/com/unsignedcharacters/dassie/MainActivity.kt
@@ -14,6 +14,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference()
+        val userRef = ref.child("users")
+
+        val userListener = object: ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                val users = dataSnapshot.children.iterator()
+
+                while (users.hasNext()) {
+
+                    val currentUser = users.next()
+
+                    val user = currentUser.getValue() as HashMap<String, Any>
+
+                    val userId = currentUser.key
+                    val userName: String? = user.get("name") as? String
+
+                    text_name.text = userName
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+        }
+
+        userRef.addValueEventListener(userListener)
 
         button_borrow.setOnClickListener {
 
