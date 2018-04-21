5cab4aed5647e92ead7e811fc867099bf559f959
Brandon Addison <addbra001@myuct.ac.za>
2018-04-21 05:03:01 +0200

    Profile and Edit Profile UI complete

--- app/src/main/java/com/unsignedcharacters/dassie/ProfileActivity.kt
+++ app/src/main/java/com/unsignedcharacters/dassie/ProfileActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 import kotlinx.android.synthetic.main.activity_profile.*
@@ -13,7 +14,11 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        val user = FirebaseAuth.getInstance().currentUser
 
+        if (user != null) {
+            fullName.text = user.displayName
+        }
         button_edit.setOnClickListener {
 
             val intent = Intent(this, EditProfileActivity::class.java)
